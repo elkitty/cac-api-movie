94c6b1be7c5b53e76b2617e7b2e3cb62f67040cb
elkitty <hablaconwafle@gmail.com>
2024-07-07 14:35:56 -0300

    clase 29

--- db/db.js
+++ db/db.js
@@ -1,23 +1,20 @@
-const mysql = require('mysql2');
+// Cargar las variables de entorno desde el archivo .env
+require("dotenv").config();
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'mysqlroot',
-    database: process.env.DB_NAME
-})
-
-// host: process.env.DB_ROOT,
-// user: process.env.DB_USER,
-// password: process.env.DB_PASS,
-// database: process.env.DB_NAME
+  host: process.env.DB_ROOT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
 
 connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to database', err);
-        return;
-    }
-    console.log('Connected to the database')
+  if (err) {
+    console.error("Error connecting to database", err);
+    return;
+  }
+  console.log("Connected to the database");
 });
 
 module.exports = connection;
